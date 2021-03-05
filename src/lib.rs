364e2275f47081f0d9b5fd60c762856a92c18f2f
Genwald <sethv27@gmail.com>
2021-03-05 14:02:53 -0600

    log errors and try again

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,14 @@ const PORT: u16 = 43022;
 #[skyline::main(name = "arc-network")]
 pub fn main() {
     std::thread::spawn(||{
-        Networked::new_buffered(File::open("rom:/data.arc").unwrap(), ("0.0.0.0", PORT))
-            .unwrap()
-            .listen()
-            .unwrap();
+        loop {
+            match Networked::new_buffered(File::open("rom:/data.arc").unwrap(), ("0.0.0.0", PORT))
+                .unwrap()
+                .listen()
+                {
+                    Err(e) => println!("[arc-network] Connection error: {:?}", e),
+                    Ok(i) => i,
+                }
+        }
     });
 }
